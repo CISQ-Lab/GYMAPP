781c3f4c878843a532e41e8dad1e22ddb74f1fce
Christian S <cisq2002@outlook.com>
2026-08-10 17:58:30 -0600

    Se añadio un router para comenzar a separar y administrar las rutas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {dirname, join}from "path";
 import { fileURLToPath } from "url";
+import indexRouter from "./routes/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,11 +12,9 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", join(__dirname, "views"));
+app.use(indexRouter);
 app.listen(port);
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "My Express App" });
-}); 
 
 
 console.log(`Server is running on port ${port}`);
