9f88eb93b5b99acc326f4eeb8f2b19ee1646d72c
Christian Sanchez <cisq2002@outlook.com>
2026-09-16 00:36:00 -0600

    Agrego spinner para la pagina members cuando aun no cargan

--- frontend/src/pages/gymPages/Members.jsx
+++ frontend/src/pages/gymPages/Members.jsx
@@ -1,4 +1,3 @@
-import StatCard from "../../components/cards/StatCard";
 import Button from "../../components/buttons/button";
 import { NavLink } from "react-router-dom";
 import Membercard from "../../components/cards/MemberCard";
@@ -6,44 +5,38 @@ import { useState, useEffect } from "react";
 import useGym from "../../hooks/useGym"
 import { apiFetch } from "../../services/api";
 import showError from "../../components/messages/showError"
+import Spinner from "../../components/layout/spinner";
 
 function Members() {
 
     const { gym } = useGym();
     const [members, setMembers] = useState([])
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        if(gym?.id === null){
+        if (gym?.id === null) {
             return
         }
 
         const getMembers = async () => {
-            try{
+            try {
                 const data = await apiFetch(`/gyms/${gym?.id}/getMembers`);
-                data.success && setMembers(data.members); 
-            }catch(error){
+                data.success && setMembers(data.members);
+                setLoading(false);
+            } catch (error) {
                 showError(error.message);
             }
-            
+
 
         }
 
-        if(gym?.id){
+        if (gym?.id) {
             getMembers();
-        } 
+        }
 
 
     }, [gym?.id])
 
-    if(members.length !== 0) console.log(members);
- 
-    const stats = [
-        { title: "Miembros", value: "150" },
-        { title: "Entrenadores", value: "10" },
-        { title: "Planes", value: "5" },
-        { title: "Productos", value: "20" },
-        { title: "Pagos del mes", value: "$5000" }
-    ];
 
     return (
         <>
@@ -55,16 +48,20 @@ function Members() {
 
             </div>
 
+            {loading ? <Spinner isLoading={true} /> :
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {
+                        members.map((member) => (
+                            <Membercard key={member.id} member={member} />
+                        ))
+                    }
 
-                {
-                    members.map((member) => (
-                        <Membercard key={member.id} member={member}  />
-                    ))
-                }
+                </div>
+            }
 
-            </div>
+
+
         </>
 
 
